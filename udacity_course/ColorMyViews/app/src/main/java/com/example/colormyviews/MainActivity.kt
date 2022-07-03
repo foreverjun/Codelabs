9f01c4b8063402f42a470835b225b717cbb38aa9
matvey <st090604@student.spbu.ru>
2022-07-03 12:18:45 +0300

    Constraint Layout training app
    An app in which the basic methods of working with Constraint Layout were used

---
+++ udacity_course/ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.colormyviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(
+                binding.boxOneText,
+                binding.boxTwoText,
+                binding.boxThreeText,
+                binding.boxFourText,
+                binding.boxFiveText,
+                binding.constraintLayout,
+                binding.redButton,
+                binding.yellowButton,
+                binding.greenButton
+            )
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_green)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_yellow)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
